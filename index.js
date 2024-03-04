ed56455f816420403a952b3ea81557123691c7df
ChalChalCh.... <k.calvin567@gmail.com>
2024-03-04 15:57:49 +0530

    login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const quizzes = require('./db/quizzes');
 const cors = require('cors');
 const quizRouter = require("./router/quiz.router");
 const userData = require('./db/users');
-const {loginRouter,signupRouter} = require('./controllers/authController');
+const loginRouter = require('./controllers/authController');
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.get('/',(req,res) =>{
     res.send('Hello Borld!')
 })
 
-app.post("/auth/login", loginRouter);
+app.use("/auth/login", loginRouter);
 //app.use("/auth/signup", signupRouter);
 app.use("/quiz",quizRouter);
 
--- router/authrouter.js
+++ router/authrouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
-const {loginHandler,signupHandler} = require('../controllers/authController ');
+const {loginHandler,signupHandler} = require('../controllers/authController');
 
 const loginRouter = express.Router();
 const signupRouter = express.Router();
 
-loginRouter.route('/')
+loginRouter.route("/")
 .post(loginHandler)
 
-signupRouter.route('/')
+signupRouter.route("/")
 .post(signupHandler)
 
 module.exports = {loginRouter,signupRouter};
