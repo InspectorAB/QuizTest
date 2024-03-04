8dafb6c65da86c8a2ede08fd66c03f6cefcdcb73
ChalChalCh.... <k.calvin567@gmail.com>
2024-03-04 16:23:49 +0530

    router

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,17 +1,14 @@
 const express = require('express');
 
-const {loginHandler,signupHandler} = require('../controllers/authController');
+const {loginHandler, signupHandler} = require("../controllers/authController");
 
 const loginRouter = express.Router();
 const signupRouter = express.Router();
 
 loginRouter.route("/")
-    .post((req, res, next) => {
-        // Middleware logic here if needed
-        loginHandler(req, res, next); // Call the controller function within the middleware
-    });
-    
-signupRouter.route("/")
-.post(signupHandler)
+    .post(loginHandler)
 
-module.exports = {loginRouter,signupRouter};
+signupRouter.route("/")
+    .post(signupHandler)
+
+module.exports = { loginRouter, signupRouter };
