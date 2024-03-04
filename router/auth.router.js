cb8e0d11faf08b358a23c69560abf2ce656e47e4
ChalChalCh.... <k.calvin567@gmail.com>
2024-03-04 16:34:13 +0530

    hehe

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,25 +1,14 @@
 const express = require('express');
 
-const loginHandler = require("../controllers/authController");
+const {loginHandler, signupHandler} = require("../controllers/authController");
 
 const loginRouter = express.Router();
 const signupRouter = express.Router();
 
-// loginRouter.route("/")
-//     .post(loginHandler)
+loginRouter.route("/")
+    .post(loginHandler)
 
-loginRouter.post("/", (req, res) => {
-    const { username, password } = req.body;
-    const isUserVerified = userdata.data.some(user => user.username === username && user.password === password);
-    if (isUserVerified) {
-        const token = jwt.sign({ id: username }, process.env.SECRET_TOKEN);
-        res.json({ username, token, message: "User Verified" });
-    } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-    }
-});
+signupRouter.route("/")
+    .post(signupHandler)
 
-// signupRouter.route("/")
-//     .post(signupHandler)
-
-module.exports = { loginRouter };
+module.exports = { loginRouter, signupRouter };
--- router/quiz.router.js
+++ router/quiz.router.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const quizRouter = express.Router();
-const quizzes = require('../db/quizzes');
+// const express = require("express");
+// const quizRouter = express.Router();
+// const quizzes = require('../db/quizzes');
+// const  authVerify  = require("../middleware/authverify");
 
+// quizRouter.route("/")
+//     .get(authVerify, (req, res) => {
+//         res.json(quizzes)
+//     });
 
-quizRouter.route("/")
-.get((req,res) =>{
-    // when we send some data we also send it as a json
-    res.json(quizzes.data)
-});
-
-module.exports = quizRouter;
+// module.exports = quizRouter;
