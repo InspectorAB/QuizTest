39c66ed68f00098a33e098edd2c25e62ca125445
ChalChalCh.... <k.calvin567@gmail.com>
2024-03-04 11:35:47 +0530

    json

--- router/quiz.router.js
+++ router/quiz.router.js
@@ -2,9 +2,11 @@ const express = require("express");
 const quizRouter = express.Router();
 const quizzes = require('../db/quizzes');
 
+
 quizRouter.route("/")
 .get((req,res) =>{
-    res.send(quizzes.data)
+    // when we send some data we also send it as a json
+    res.json(quizzes.data)
 });
 
 module.exports = quizRouter;
