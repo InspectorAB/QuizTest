09351dbb34c9cdf9a6745c69be22ddeb5c7cac99
ChalChalCh.... <k.calvin567@gmail.com>
2024-03-04 11:33:44 +0530

    Router

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const quizzes = require('./db/quizzes');
+const cors = require('cors');
+const quizRouter = require("./router/quiz.router");
 
 const app = express();
 
 const port = 3000
+app.use(cors());
 
 app.get('/',(req,res) =>{
     res.send('Hello Borld!')
 })
 
-app.get("/quiz",(req,res) =>{
-    res.send(quizzes.data)
-})
+app.use("/quiz",quizRouter);
+
+
+// dont need this now, routing through a quizRouter
+// app.get("/quiz",(req,res) =>{
+//     res.send(quizzes.data)
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
