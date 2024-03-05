59cf32957127d0a9fa36cf30b7b2097cda0e0403
ChalChalCh.... <k.calvin567@gmail.com>
2024-03-05 10:22:46 +0530

    MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,8 @@ const { v4: uuid } = require("uuid");
 
 const userdata = require("../db/users");
 
+const getUserData = require("../db/users");
+
 
 
 const signupHandler = (req, res) => {
@@ -21,15 +23,37 @@ const signupHandler = (req, res) => {
     }
 }
 
-const loginHandler = (req, res) => {
-    const { username, password } = req.body;
-        const isUserVerified = userdata.users.some(user => user.username === username && user.password === password);
+// const loginHandler = (req, res) => {
+//     const { username, password } = req.body;
+//         const isUserVerified = userdata.users.some(user => user.username === username && user.password === password);
+//         if(isUserVerified){
+//             const token = jwt.sign({id: username}, process.env.SECRET_TOKEN)
+//             res.json({username, token, message: "User Verfied"})
+//         }else{
+//             res.status(401).json({message: "Invalid Credentials"})
+//         }
+// }
+
+
+const loginHandler = async(req,res) =>{
+    try{
+        const{username,password} = req.body;
+
+        const userData = await getUserData();
+
+        const isUserVerified = userData.some(user => user.username === username && user.password === password);
+
         if(isUserVerified){
-            const token = jwt.sign({id: username}, process.env.SECRET_TOKEN)
-            res.json({username, token, message: "User Verfied"})
+            const token = jwt.token({id:username},process.env.SECRET_TOKEN);
+            res.json({username,token,message:"User Verifed Using MongoDB"});
         }else{
-            res.status(401).json({message: "Invalid Credentials"})
+            res.status(401).json({message: "Invalid credentails Using Mongo DB"});
         }
+        
+    }catch(error){
+        console.error("Error in loginhandler");
+        res.status(500).json({message: "Internal server error", error:error.message});
+    }
 }
 
 module.exports = { loginHandler, signupHandler };;
--- db/users.js
+++ db/users.js
@@ -1,4 +1,57 @@
 const { v4: uuid } = require("uuid");
+const {MongoClient} = require('mongodb');
+
+const uri = "mongodb://localhost:27017";
+
+const dbName = "Users";
+
+const client = new MongoClient(uri,{useNewUrlParser: true,useUnifiedTopology:true});
+
+async function connectToMongoDB(){
+    try{
+        await client.connect();
+        console.log("connected");
+
+        const db = client.db(dbName);
+
+        return db;
+    }catch(error){
+        console.error("Error connecting to MongoDB:",error);
+        throw error;
+    }
+}
+
+async function getUserData(){
+    try{
+        const db = await connectToMongoDB();
+
+        const collection = db.collection("users");
+
+        const userData = await collection.find().toArray();
+
+        if(userData.length === 0){
+            await collection.insertMany([
+                {
+                    id: uuid(),
+                    username: "aditya",
+                    password: "123456789",
+                    emailId: "ab@gmail.com",
+                },
+                {
+                    id: uuid(),
+                    username: "bhaskar",
+                    password: "987654321",
+                    emailId: "ba@gmail.com",
+                },
+            ]);
+        }
+
+        return JSON.stringify(userData);
+    }catch(error){
+        console.error("error retrieiving");
+        throw error;
+    }
+}
 
 const userdata = {
     "users": [
@@ -17,4 +70,4 @@ const userdata = {
     ]
 }
 
-module.exports = userdata;
+module.exports = getUserData;
